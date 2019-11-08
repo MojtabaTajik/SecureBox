e9237cd32658ade19148303c1460dfc5e2fbe361
Felony <onyfel@gmail.com>
2019-11-08 22:37:52 +0330

    Add DriveLetter class
    Add DriveLetter class which help find first unused drive letter

---
+++ SecureBox/VirtualDrive/Core/DriveLetter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace VirtualDrive.Core
+{
+    public static class DriveLetter
+    {
+        public static string UnusedDriveLetter()
+        {
+            var validLetters = Enumerable.Range('A', 'Z').Select(c => (char) c).ToList();
+
+            var usedDriveLetters = UsedDriveLetters();
+
+            foreach (char letter in validLetters)
+            {
+                if (!usedDriveLetters.Contains(letter))
+                    return letter.ToString();
+            }
+
+            throw new ArgumentException("");
+        }
+
+        private static List<char> UsedDriveLetters()
+        {
+            return DriveInfo.GetDrives().Select(s => s.Name[0]).ToList();
+        }
+    }
+}
