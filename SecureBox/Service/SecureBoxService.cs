938bfcd49d524196a973ad1c88500f5c4af6a433
Felony <onyfel@gmail.com>
2019-11-08 22:59:20 +0330

    Mount & unmount virtual drive on service start & stop

--- SecureBox/Service/SecureBoxService.cs
+++ SecureBox/Service/SecureBoxService.cs
@@ -1,20 +1,47 @@
 ﻿using Microsoft.Extensions.Hosting;
-using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace Service
 {
     public class SecureBoxService : IHostedService
     {
+        private readonly ILogger<SecureBoxService> _logger;
+        private readonly VirtualDrive.VirtualDrive _virtualDrive;
+
+        public SecureBoxService(ILogger<SecureBoxService> logger, VirtualDrive.VirtualDrive virtualDrive)
+        {
+            _logger = logger;
+            _virtualDrive = virtualDrive;
+        }
+
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            _logger.LogInformation("Service start");
+
+            var startTask = new Task(() =>
+            {
+                _virtualDrive.MoundVirtualDrive();
+            }, cancellationToken);
+
+            startTask.Start();
+
+            return startTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            _logger.LogInformation("Service Stop");
+
+            var stopTask = new Task(() =>
+            {
+                _virtualDrive.UnmountVirtualDrive();
+            }, cancellationToken);
+
+            stopTask.Start();
+
+            return stopTask;
         }
     }
 }
