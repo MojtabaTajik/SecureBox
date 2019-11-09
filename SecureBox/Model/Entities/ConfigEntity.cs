a0c26ddeaf07acbc45db6299bb082f39bc53d44c
Mojtaba Tajik <onyfel@gmail.com>
2019-11-09 14:27:07 +0330

    Add ProtectMode

--- SecureBox/Model/Entities/ConfigEntity.cs
+++ SecureBox/Model/Entities/ConfigEntity.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using Shared;
 
 namespace Model.Entities
 {
     public class ConfigEntity : BaseEntity
     {
+        public ProtectMode ProtectMode { get; set; }
         public List<string> WhiteListExtensions { get; set; }
     }
 }
