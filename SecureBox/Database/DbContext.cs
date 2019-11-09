874bea9d6247c3f06e0d5d1f83798a2af88e416d
Mojtaba Tajik <onyfel@gmail.com>
2019-11-09 15:06:07 +0330

    Add ConfigOperations
    - Add AddConfig() method to store configuration

--- SecureBox/Database/DbContext.cs
+++ SecureBox/Database/DbContext.cs
@@ -1,14 +1,21 @@
-﻿using LiteDB;
+﻿using System.IO;
+using Data.Operations;
+using Data.Properties;
+using LiteDB;
+using Shared.Utils;
 
 namespace Data
 {
     public class DbContext
     {
-        private readonly LiteDatabase _database;
+        public ConfigOperations Config { get; set; }
 
         public DbContext()
         {
-            _database = new LiteDatabase("Config.dll");
+            string dbPath = Path.Combine(PathUtils.ConfigPath(), Resources.DBFileName);
+            var database = new LiteDatabase(dbPath);
+
+            Config = new ConfigOperations(database);
         }
     }
 }
