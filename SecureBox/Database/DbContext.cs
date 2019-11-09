6db0f3c200ba8f3fc6c5cf4e1861a015220131c8
Mojtaba Tajik <onyfel@gmail.com>
2019-11-09 14:45:58 +0330

    Add Data project

---
+++ SecureBox/Database/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using LiteDB;
+
+namespace Data
+{
+    public class DbContext
+    {
+        private readonly LiteDatabase _database;
+
+        public DbContext()
+        {
+            _database = new LiteDatabase("Config.dll");
+        }
+    }
+}
